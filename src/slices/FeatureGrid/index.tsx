448d3a3672b216a82780c6c5021456c7b12dc175
aldjonz <ajon4004@gmail.com>
2025-01-22 09:09:37 +0000

    Basic feature page setup

--- src/slices/FeatureGrid/index.tsx
+++ src/slices/FeatureGrid/index.tsx
@@ -1,22 +1,29 @@
 import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
+import { JSX } from "react";
 
-/**
- * Props for `FeatureGrid`.
- */
 export type FeatureGridProps = SliceComponentProps<Content.FeatureGridSlice>;
 
-/**
- * Component for "FeatureGrid" Slices.
- */
 const FeatureGrid = ({ slice }: FeatureGridProps): JSX.Element => {
+  console.log(slice)
   return (
     <section
+      id="features"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="w-full h-screen bg-white"
     >
-      Placeholder component for feature_grid (variation: {slice.variation})
-      Slices
+      <p className="text-2xl font-bold text-center text-4xl">{slice.primary.title}</p>
+      <div className="grid grid-cols-2 gap-4 px-28">  
+        {slice.primary.cards.map((card, index) => (
+          <div key={index} className="flex flex-col gap-4">
+            <PrismicNextImage field={card.icon} className="w-14 h-14 object-cover self-center" />
+            <p className="text-2xl font-bold">{card.title}</p>
+            <p className="text-2xl">{card.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
