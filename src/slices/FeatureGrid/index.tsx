b52c8909386108f0fb4f06b4705d6ef1d5b98bf1
aldjonz <ajon4004@gmail.com>
2025-01-24 09:56:29 +0000

    Add styling to feature cards

--- src/slices/FeatureGrid/index.tsx
+++ src/slices/FeatureGrid/index.tsx
@@ -11,12 +11,12 @@ const FeatureGrid = ({ slice }: FeatureGridProps): JSX.Element => {
       id="features"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="w-full h-screen bg-white"
+      className="w-full h-screen bg-white mt-32"
     >
-      <p className="text-2xl font-bold text-center text-4xl">{slice.primary.title}</p>
-      <div className="grid grid-cols-2 gap-4 px-28">  
+      <p className="text-2xl font-bold text-center text-4xl mb-8">{slice.primary.title}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:px-56 px-8">  
         {slice.primary.cards.map((card, index) => (
-          <div key={index} className="flex flex-col gap-4">
+          <div key={index} className="flex flex-col gap-4 shadow-xl ring-2 ring-gray-100 p-4 rounded-xl">
             <PrismicNextImage field={card.icon} className="w-14 h-14 object-cover self-center" />
             <p className="text-2xl font-bold">{card.title}</p>
             <p className="text-2xl">{card.description}</p>
