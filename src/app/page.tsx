2d5cb877fbd3f576ae75a084230769f49fcfc433
aldjonz <ajon4004@gmail.com>
2025-01-24 11:55:20 +0000

    Updated footer links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/aldjonz/encord"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -37,11 +37,11 @@ export default async function Home() {
             width={16}
             height={16}
           />
-          Learn
+          GitHub
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://encord-technical-challenge.prismic.io/builder/working"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -52,22 +52,7 @@ export default async function Home() {
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Prismic
         </a>
       </footer>
     </div>
